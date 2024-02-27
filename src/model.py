d39db1a8cf1b68205d6958ed68ca29e68b9c6a9f
johannsc <johann.schmidt@ovgu.de>
2024-02-27 09:14:25 +0100

    custom resnext block

--- src/model.py
+++ src/model.py
@@ -72,7 +72,7 @@ class GroupConv2D(tf.keras.layers.Layer):
     def call(self, inputs, **kwargs):
         feature_map_list = []
         for i in range(self.groups):
-            x_i = self.conv_list[i](inputs[:, :, :, i*self.group_in_num: (i + 1) * self.group_in_num])
+            x_i = self.conv_list[i](inputs[:, :, :, i * self.group_in_num: (i + 1) * self.group_in_num])
             feature_map_list.append(x_i)
         out = tf.concat(feature_map_list, axis=-1)
         return out
@@ -81,24 +81,24 @@ class GroupConv2D(tf.keras.layers.Layer):
 class ResNeXt_BottleNeck(tf.keras.layers.Layer):
     def __init__(self, filters, strides, groups):
         super(ResNeXt_BottleNeck, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(filters=filters,
+        self.conv1 = tf.keras.layers.Conv2D(filters=filters//2 if filters > 1 else filters,
                                             kernel_size=(1, 1),
                                             strides=1,
                                             padding="same")
         self.bn1 = tf.keras.layers.BatchNormalization()
-        self.group_conv = GroupConv2D(input_channels=filters,
-                                      output_channels=filters,
+        self.group_conv = GroupConv2D(input_channels=filters//2 if filters > 1 else filters,
+                                      output_channels=filters//2 if filters > 1 else filters,
                                       kernel_size=(3, 3),
                                       strides=strides,
                                       padding="same",
                                       groups=groups)
         self.bn2 = tf.keras.layers.BatchNormalization()
-        self.conv2 = tf.keras.layers.Conv2D(filters=2 * filters,
+        self.conv2 = tf.keras.layers.Conv2D(filters=filters,
                                             kernel_size=(1, 1),
                                             strides=1,
                                             padding="same")
         self.bn3 = tf.keras.layers.BatchNormalization()
-        self.shortcut_conv = tf.keras.layers.Conv2D(filters=2 * filters,
+        self.shortcut_conv = tf.keras.layers.Conv2D(filters=filters,
                                                     kernel_size=(1, 1),
                                                     strides=strides,
                                                     padding="same")
@@ -107,92 +107,19 @@ class ResNeXt_BottleNeck(tf.keras.layers.Layer):
     def call(self, inputs, training=None, **kwargs):
         x = self.conv1(inputs)
         x = self.bn1(x, training=training)
-        x = tf.nn.relu(x)
+        x = tf.nn.gelu(x)
         x = self.group_conv(x)
         x = self.bn2(x, training=training)
-        x = tf.nn.relu(x)
+        x = tf.nn.gelu(x)
         x = self.conv2(x)
         x = self.bn3(x, training=training)
-        x = tf.nn.relu(x)
+        x = tf.nn.gelu(x)
 
         shortcut = self.shortcut_conv(inputs)
         shortcut = self.shortcut_bn(shortcut, training=training)
 
-        output = tf.nn.relu(tf.keras.layers.add([x, shortcut]))
+        output = tf.nn.gelu(tf.keras.layers.add([x, shortcut]))
         return output
-
-
-def build_ResNeXt_block(filters, strides, groups, repeat_num):
-    block = tf.keras.Sequential()
-    block.add(ResNeXt_BottleNeck(filters=filters,
-                                 strides=strides,
-                                 groups=groups))
-    for _ in range(1, repeat_num):
-        block.add(ResNeXt_BottleNeck(filters=filters,
-                                     strides=1,
-                                     groups=groups))
-
-    return block
-
-
-class ResNeXt(tf.keras.Model):
-    def __init__(self, repeat_num_list, cardinality):
-        if len(repeat_num_list) != 4:
-            raise ValueError("The length of repeat_num_list must be four.")
-        super(ResNeXt, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(filters=64,
-                                            kernel_size=(7, 7),
-                                            strides=2,
-                                            padding="same")
-        self.bn1 = tf.keras.layers.BatchNormalization()
-        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=(3, 3),
-                                               strides=2,
-                                               padding="same")
-        self.block1 = build_ResNeXt_block(filters=128,
-                                          strides=1,
-                                          groups=cardinality,
-                                          repeat_num=repeat_num_list[0])
-        self.block2 = build_ResNeXt_block(filters=256,
-                                          strides=2,
-                                          groups=cardinality,
-                                          repeat_num=repeat_num_list[1])
-        self.block3 = build_ResNeXt_block(filters=512,
-                                          strides=2,
-                                          groups=cardinality,
-                                          repeat_num=repeat_num_list[2])
-        self.block4 = build_ResNeXt_block(filters=1024,
-                                          strides=2,
-                                          groups=cardinality,
-                                          repeat_num=repeat_num_list[3])
-        self.pool2 = tf.keras.layers.GlobalAveragePooling2D()
-        self.fc = tf.keras.layers.Dense(units=NUM_CLASSES,
-                                        activation=tf.keras.activations.softmax)
-
-    def call(self, inputs, training=None, mask=None):
-        x = self.conv1(inputs)
-        x = self.bn1(x, training=training)
-        x = tf.nn.relu(x)
-        x = self.pool1(x)
-
-        x = self.block1(x, training=training)
-        x = self.block2(x, training=training)
-        x = self.block3(x, training=training)
-        x = self.block4(x, training=training)
-
-        x = self.pool2(x)
-        x = self.fc(x)
-
-        return x
-
-
-def ResNeXt50():
-    return ResNeXt(repeat_num_list=[3, 4, 6, 3],
-                   cardinality=32)
-
-
-def ResNeXt101():
-    return ResNeXt(repeat_num_list=[3, 4, 23, 3],
-                   cardinality=32)
 
 
 class MNISTClassifier(tf.keras.layers.Layer):
